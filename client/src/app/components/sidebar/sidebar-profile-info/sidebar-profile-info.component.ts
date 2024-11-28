0aebcf6c4a642f41131d1bf97a18fb21636451ca
Lucky-Martin Matev <luckymatev@proton.me>
2024-11-28 16:28:07 +0200

    feat(sidebar): Add profile-info component

--- client/src/app/components/sidebar/sidebar-profile-info/sidebar-profile-info.component.ts
+++ client/src/app/components/sidebar/sidebar-profile-info/sidebar-profile-info.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { IUser } from '../../../models/IUser';
+import { NgIf } from '@angular/common';
+import { SpinnerComponent } from "../../spinner/spinner.component";
 
 @Component({
   selector: 'app-sidebar-profile-info',
   standalone: true,
-  imports: [],
+  imports: [NgIf, SpinnerComponent],
   templateUrl: './sidebar-profile-info.component.html',
   styleUrl: './sidebar-profile-info.component.scss'
 })
 export class SidebarProfileInfoComponent {
   constructor(private authService: AuthService) { }
 
-  public getUsername(): string {
-    return this.authService.username;
+  public getUser(): IUser {
+    return this.authService.user;
   }
 }
